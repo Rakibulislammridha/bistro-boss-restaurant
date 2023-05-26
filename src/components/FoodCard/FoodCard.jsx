fbca87f267a78f81d63f8365e717b791a732834a
Rakibulislammridha <dr.rakibulislammridha@gmail.com>
2023-05-27 01:42:38 +0600

    get dada from from server site

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -13,11 +13,11 @@ const FoodCard = ({item}) => {
         />
       </figure>
       <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
-      <div className="card-body">
+      <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Add To Cart</button>
+          <button className="btn btn-outline border-0 border-orange-400 border-b-4 mt-4 bg-slate-200 text-black">Add To Cart</button>
         </div>
       </div>
     </div>
